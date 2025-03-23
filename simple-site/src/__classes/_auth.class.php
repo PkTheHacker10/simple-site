0bf5c4dd062b191a42ffca8a15287e135099893c
pk <pk_server@pkserver@quickcom.com>
2025-03-23 07:04:09 +0000

    auth documents added

--- simple-site/src/__classes/_auth.class.php
+++ simple-site/src/__classes/_auth.class.php
@@ -3,6 +3,17 @@
 class auth
 {
     public static $user_data = [];
+
+    /**
+     * Authenticate a user based on the provided password.
+     *
+     * This function checks if the query result contains a user and verifies the password.
+     * If authentication is successful, it returns the username. Otherwise, it returns false.
+     *
+     * @param object $result The database query result containing user data.
+     * @param string $password The password entered by the user.
+     * @return mixed The username if authentication is successful, otherwise false.
+     */
     public static function login($result, $password)
     {
         if ($result->num_rows > 0) {
@@ -17,6 +28,19 @@ class auth
             return false;
         }
     }
+    /**
+     * Register a new user in the database.
+     *
+     * Calls the `insert_user()` function from the `db` class to store user details.
+     * Returns true if successful, otherwise false.
+     *
+     * @param string $name The full name of the user.
+     * @param string $username The chosen username.
+     * @param string $email The user's email address.
+     * @param string $phone The user's phone number.
+     * @param string $password The user's password.
+     * @return bool True if registration is successful, otherwise false.
+     */
     public static function signup($name, $username, $email, $phone, $password)
     {
         $result = db::insert_user($name, $username, $email, $phone, $password);
@@ -26,10 +50,19 @@ class auth
             return false;
         }
     }
+
+    /**
+     * Log out the currently authenticated user.
+     *
+     * Redirects to the login page.
+     *
+     * @return void
+     */
     public static function logout()
     {
         unset($_SESSION['auth_token']);
         header("Location: login.php");
+        exit();
     }
 
 }
